d16709bd393eaeaf5968f726df4fe31a16607b44
Nafi <nafines007@gmail.com>
2024-09-13 01:52:43 +0530

    bbbcntr_rust as a separate git repo

--- bbbcntr_rust/migrations/20240911084908_clients.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-CREATE TABLE clients (
-    client_id  SERIAL PRIMARY KEY,
-    domain varchar(511) NOT NULL UNIQUE,
-    email varchar(255) NOT NULL UNIQUE,
-    detail text NOT NULL,
-    created_on timestamp with time zone NOT NULL,
-    updated_on timestamp with time zone NOT NULL
-);
--- bbbcntr_rust/migrations/20240912162045_pending_clients.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-CREATE TABLE pending_clients (
-    pending_clients_id  SERIAL PRIMARY KEY,
-    domain varchar(511) NOT NULL UNIQUE,
-    email varchar(255) NOT NULL UNIQUE,
-    detail text NOT NULL,
-    created_on timestamp with time zone NOT NULL
-);
